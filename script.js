80a18e9018dd17373d0da3822a5bce8081a99ca4
Mathew Garcia <mathewegarcia@gmail.com>
2023-07-17 02:49:28 -0700

    mathew garcia

---
+++ script.js
@@ -0,0 +1,16 @@
+// const navbar = document.querySelector('.navbar');
+// navbar.addEventListener('mouseover', () => {
+//   navbar.animate([{ width: '5rem' }, { width: '16rem' }], {
+//     duration: 500,
+//     fill: 'forwards',
+//     easing: 'ease-in-out',
+//   });
+// });
+
+// navbar.addEventListener('mouseout', () => {
+//   navbar.animate([{ width: '16rem' }, { width: '5rem' }], {
+//     duration: 500,
+//     fill: 'forwards',
+//     easing: 'ease-in-out',
+//   });
+// });
